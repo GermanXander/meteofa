1a21323ace601ea828a3b87ca29e34e0973c9c1a
Germán Xander <german.xander@fio.unam.edu.ar>
2023-11-23 08:20:42 -0300

    using irq

--- main.py
+++ main.py
@@ -1,57 +1,58 @@
-from machine import Pin
+from machine import Pin, Timer
+from time import ticks_us, ticks_diff
+import uasyncio as asyncio
 import json
 from mqtt_as import MQTTClient, config
-import uasyncio as asyncio
-from primitives.switch import Switch
-from time import ticks_diff, ticks_us
-from settings import SERVIDOR, SSID, PASS
+from settings import SERVIDOR, SSID, WIFI_PASS, MQTT_USR, MQTT_USR_PASS
 
 config['server'] = SERVIDOR
 config['ssid'] = SSID
-config['wifi_pw'] = PASS
+config['wifi_pw'] = WIFI_PASS
 # config['connect_coro'] = conn_han
 # config['wifi_coro'] = wifi_han
 config['ssl'] = True
+config['user'] = USR_PASS
+config['password'] = MQTT_USR_PASS
 
 # Set up client
 MQTTClient.DEBUG = False  # Optional
 client = MQTTClient(config)
-
-tstart = 0.000000000001
-delta = 0
-contador = 0
-
-# Callback on debounced switch closure
-def timeit():
-    global tstart, delta, contador
-    contador += 1
-    delta += ticks_diff(ticks_us(), tstart)
+class AsyncPin:
+    contador = 0
     tstart = ticks_us()
+    delta = 0
 
-async def main(client):
-    await client.connect()
-    n = 0
-    await asyncio.sleep(2)  # Give broker time
-    global delta, contador
+    def __init__(self, pin, trigger):
+        self.pin = pin
+        self.flag = asyncio.ThreadSafeFlag()
+        self.pin.irq(handler=lambda pin: self.flag.set(), trigger=trigger)
+
+    async def wait_edge(self):
+        await self.flag.wait()
+        diferencia = ticks_diff(ticks_us(), self.tstart)
+        if diferencia > 100 or self.contador == 0:
+            # print('Got edge.')
+            self.delta += diferencia
+            self.tstart = ticks_us()
+            self.contador += 1
+
+pin_in = Pin(22, Pin.IN, Pin.PULL_DOWN)
+async_pin = AsyncPin(pin_in, Pin.IRQ_RISING)
+
+async def foo():
     while True:
-        if contador > 0:
-            # apply scaling
-            velocidad=1000000*contador/delta
-            delta = 0
-            contador = 0
-            print(velocidad)  # To test, just print ms
-            datos=json.dumps(
-                        ('velocidad',velocidad)
-                    )
-            await client.publish("meteorologica/", datos, qos = 1)
-        await asyncio.sleep(5)  # Report once per second
+        await async_pin.wait_edge()
 
-sensor = Pin(22, Pin.IN)  # Hardware: assumes switch to gnd
-sw = Switch(sensor)
-sw.close_func(timeit)  # Associate callback with switch contact closure
+async def main():
+    asyncio.create_task(foo())
+    while True:
+        await asyncio.sleep(1.5)
+        if async_pin.contador > 1:
+            promedio = 1000000*(async_pin.contador)/async_pin.delta
+        else:
+            promedio = None
+        print(promedio)
+        async_pin.delta = 0
+        async_pin.contador = 0
 
-try:
-    asyncio.run(main(client))
-finally:
-    client.close()
-    asyncio.new_event_loop()
+asyncio.run(main())
